742fa7d25568c6e1b03fb3e245908b0e84fe7d64
robsonAD <robsonfariasAD@gmail.com>
2021-03-15 15:21:11 -0300

    Subindo projeto para o git

---
+++ src/components/Final.vue
@@ -0,0 +1,51 @@
+<template>
+    <div class="final">
+        <div class="final-texto">
+            <span :class="`${etapa}`">{{texto}}</span>
+        </div>
+
+        <button 
+            class="final-botao"
+            v-on:click="jogarNovamente"    
+        >Jogar novamente</button>
+    </div>
+</template>
+
+<script>
+
+export default{
+    name: 'Final',
+    props: {
+        etapa: String,
+        texto: String,
+        jogarNovamente: Function
+    },
+    data() {
+        return {
+        }
+    },
+    methods: {
+        
+    },
+    components: {
+    }    
+}
+</script>
+
+<style>
+    .final-texto {
+        margin: 20px 0px;
+        font-size: 24px;
+        font-weight: bold;
+    }
+
+    .ganhador {
+        color: green;
+    }
+
+    .enforcado {
+        color: red;
+    }
+
+
+</style>
---
+++ src/components/Teclado.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class="teclado">
+        <button class="teclado-botao" 
+            v-for="(letra, key) in 'abcdefghijklmnopqrstuvxwyz'" 
+            :key="key"
+            :disable="verificarLetra(letra)"
+            v-on:click="jogar(letra)"
+            >
+            {{letra}}
+            </button>
+    </div>
+</template>
+
+<script>
+
+export default{
+    name: 'Teclado',
+    props: {
+        letras: Array,
+        verificarLetra: Function,
+        jogar: Function
+    },
+    data() {
+        return {
+        }
+    },
+    methods: {
+        
+    },
+    components: {
+    }    
+}
+</script>
+
+<style>
+    .teclado {
+        display: flex;
+        flex-wrap:wrap;
+        justify-content: center;
+        width: 85%;
+        margin-top: 20px;
+    }
+
+    .teclado-botao {
+        text-transform: uppercase;
+        margin: 5px;
+    }
+</style>
